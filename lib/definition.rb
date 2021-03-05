d597ba08fb85dfe07f9703ba840bd7a10be68c73
Jo <joannadawnmiller@gmail.com>
2021-03-05 11:55:12 -0500

    pass test to #initialize definition

---
+++ lib/definition.rb
@@ -0,0 +1,14 @@
+class Definition
+  attr_reader :id
+  attr_accessor :definition, :word_id
+
+  @@definitions = {}
+  @@total_rows = 0
+
+  def initialize(definition, word_id, id)
+    @definition = definition
+    @word_id = word_id
+    @id = id || @@total_rows += 1
+  end
+
+end
--- spec/definition_spec.rb
+++ spec/definition_spec.rb
@@ -2,3 +2,21 @@ require 'rspec'
 require 'word'
 require 'definition'
 require 'pry'
+
+describe(Definition) do
+
+  before(:each) do
+    Word.clear
+    @word = Word.new("friday", nil)
+    @word.save
+  end
+  
+  describe('#initialize') do
+    it('creates a new instance of Definition') do
+      word_definition = Definition.new("last day of the week", @word.id, nil)
+      expect(word_definition.definition).to(eq("last day of the week"))
+      expect(word_definition.word_id).to(eq(@word.id))
+    end
+  end
+
+end
