ef25ff48a0c4797277cff04a074cac7cd3f37a3d
Jo <joannadawnmiller@gmail.com>
2021-03-05 11:20:18 -0500

    pass test to initialize new Word instance

---
+++ lib/word.rb
@@ -0,0 +1,10 @@
+class Word 
+  attr_accessor :word
+
+  @@words = {}
+
+  def initialize(word)
+    @word = word
+  end  
+
+end
--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -2,3 +2,14 @@ require 'rspec'
 require 'word'
 require 'definition'
 require 'pry'
+
+describe(Word) do
+  
+  describe('#initialize') do
+    it('creates a new instance of Word') do
+      new_word = Word.new("project")
+      expect(new_word).to(eq(new_word))
+    end
+  end
+
+end
