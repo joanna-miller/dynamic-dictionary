831467aca644f4ec7ec34a668709b7002de775ab
Jo <joannadawnmiller@gmail.com>
2021-03-05 11:35:44 -0500

    pass test for #save

--- lib/word.rb
+++ lib/word.rb
@@ -1,10 +1,13 @@
 class Word 
+  attr_reader :id
   attr_accessor :word
 
   @@words = {}
+  @@total_rows = 0
 
-  def initialize(word)
+  def initialize(word, id)
     @word = word
+    @id = id || @@total_rows += 1
   end  
 
   def ==(word_to_compare)
@@ -13,6 +16,10 @@ class Word
 
   def self.all
     @@words.values
+  end 
+
+  def save
+    @@words[id] = Word.new(word, id)
   end 
 
 end
--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -7,15 +7,15 @@ describe(Word) do
   
   describe('#initialize') do
     it('creates a new instance of Word') do
-      new_word = Word.new("project")
+      new_word = Word.new("project", nil)
       expect(new_word.word).to(eq("project"))
     end
   end
 
   describe('#==') do
     it('is the same word if it has the same attributes as another word') do
-      word1 = Word.new("friday")
-      word2 = Word.new("friday")
+      word1 = Word.new("friday", nil)
+      word2 = Word.new("friday", nil)
       expect(word1).to(eq(word2))
     end
   end
@@ -23,6 +23,16 @@ describe(Word) do
   describe('.all') do
     it('returns an empty array when there are no words') do
       expect(Word.all).to(eq([]))
+    end
+  end
+
+  describe('#save') do
+    it('saves a Word instance') do
+      word1 = Word.new("friday", nil)
+      word1.save
+      word2 = Word.new("project", nil)
+      word2.save
+      expect(Word.all).to(eq([word1, word2]))
     end
   end
 
