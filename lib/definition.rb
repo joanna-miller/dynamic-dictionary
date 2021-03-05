6a4b67656b873bb794a538762065b02a3d4c17d1
Jo <joannadawnmiller@gmail.com>
2021-03-05 18:42:58 -0500

    attempt to add styling sheet, no success

--- lib/definition.rb
+++ lib/definition.rb
@@ -1,7 +1,7 @@
 class Definition
   attr_reader :id
   attr_accessor :definition, :word_id
-  
+
   @@definitions = {}
   @@total_rows = 0
 
@@ -9,7 +9,7 @@ class Definition
     @definition = definition
     @word_id = word_id
     @id = id || @@total_rows += 1
-  end
+  end 
 
   def ==(def_to_compare)
     (definition == def_to_compare.definition) && (word_id == def_to_compare.word_id)
